78491809efcde792149d5465875d237523c2c239
Aristideoued <ouedraogoaris@gmail.com>
2025-06-09 17:41:55 +0000

    gestion des cros origin

---
+++ src/main/java/com/wuri/demowuri/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.wuri.demowuri.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // URLs autorisées
+                .allowedMethods("GET", "POST", "PUT", "DELETE","UPDATE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
